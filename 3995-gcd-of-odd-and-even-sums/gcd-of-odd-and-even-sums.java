643c4440528f92e38fd778adbc93c7bf5b1a3df1
Keshav Goyal <keshavgoyal0110@gmail.com>
2026-07-15 16:07:50 +0530

    Time: 1 ms (83.11%) | Memory: 42.5 MB (57.16%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,32 +1,32 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
+        // int sumOdd = 0;
+        // int sumEven = 0;
 
-        // all odd no. b/w a particular range
-        // for (int i = 0; i <= 2*n; i++) {
-        //     if (i % 2 != 0) {
-        //         System.out.println(i);
+        // // all odd no. b/w a particular range
+        // // for (int i = 0; i <= 2*n; i++) {
+        // //     if (i % 2 != 0) {
+        // //         System.out.println(i);
+        // //     }
+        // // }
+
+        // for(int i = 1; i <= 2*n; i++){
+        //     if(i%2 == 0){
+        //         sumEven = sumEven + i;
+        //     }
+        //     else{
+        //         sumOdd = sumOdd + i;
         //     }
         // }
 
-        for(int i = 1; i <= 2*n; i++){
-            if(i%2 == 0){
-                sumEven = sumEven + i;
-            }
-            else{
-                sumOdd = sumOdd + i;
-            }
-        }
+        // while(sumEven != 0){
+        //     int rem = sumOdd%sumEven;
+        //     sumOdd = sumEven;
+        //     sumEven = rem;
+        // }
 
-        while(sumEven != 0){
-            int rem = sumOdd%sumEven;
-            sumOdd = sumEven;
-            sumEven = rem;
-        }
-
-        // System.out.println(sumOdd);
-        // System.out.println(sumEven);
+        // // System.out.println(sumOdd);
+        // // System.out.println(sumEven);
         return n;
     }
 }
